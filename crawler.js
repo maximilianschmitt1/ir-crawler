af51368accd8895b71913cc310615c384d640ac5
Maximilian Schmitt <maximilian.schmitt@stud.uni-regensburg.de>
2014-10-29 00:53:29 +0100

    optimisations

--- crawler.js
+++ crawler.js
@@ -1,13 +1,20 @@
 'use strict';
 
+var parse   = require('superagentparse');
 var request = require('superagent');
 var async   = require('async');
+var md5     = require('md5');
 
 var crawler = {
 	taskQueue: null,
+	maxQueueLength: 24000,
 	fetched: {},
 	queue: function(url) {
-		if (crawler.fetched[url]) {
+		if (crawler.hasFetched(url) || crawler.isQueued(url)) {
+			return;
+		}
+
+		if (crawler.queueLength() >= crawler.maxQueueLength) {
 			return;
 		}
 
@@ -17,19 +24,52 @@ var crawler = {
 
 		this.taskQueue.push(url);
 	},
+	hasFetched: function(url) {
+		return crawler.fetched[md5.digest_s(url)];
+	},
+	setFetched: function(url) {
+		crawler.fetched[md5.digest_s(url)] = true;
+	},
+	numFetched: function() {
+		return Object.keys(crawler.fetched).length;
+	},
+	isQueued: function(url) {
+		if (!crawler.taskQueue) {
+			return false;
+		}
+
+		for (var i = 0; i < crawler.taskQueue.tasks.length; i++) {
+			var task = crawler.taskQueue.tasks[i];
+
+			if (task.data === url) {
+				return true;
+			}
+		}
+
+		return false;
+	},
+	queueLength: function() {
+		if (!crawler.taskQueue) {
+			return 0;
+		}
+
+		return crawler.taskQueue.length();
+	},
 	fetchUrl: function(url, cb) {
-		if (crawler.fetched[url]) {
+		if (crawler.hasFetched(url)) {
 			cb();
 			return;
 		}
 
-		crawler.fetched[url] = true;
+		crawler.setFetched(url);
 
 		request
 			.get(url)
 			.accept('html')
-			.on('error', function() {
+			.parse(parse('utf-8'))
+			.on('error', function(e) {
 				console.log('!!! Error crawling ' + url);
+				console.log(e);
 				cb();
 			})
 			.end(function(res) {
--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 'use strict';
 
-var request = require('superagent');
-var cheerio = require('cheerio');
-var crawler = require('./crawler');
-var url     = require('url');
+var agent = require('webkit-devtools-agent');
+agent.start();
+
+var request     = require('superagent');
+var cheerio     = require('cheerio');
+var crawler     = require('./crawler');
+var url         = require('url');
 
 var followLinks = true;
-var numDocs   = 0;
-var numPushed = 0;
-var numQueued = 0;
-var queueMax  = 10000;
+var numDocs     = 0;
+var numPushed   = 0;
+var numQueued   = 0;
+var indexMax    = 24000;
+var domain      = /(http:\/\/|https:\/\/)(.*)\.uni-regensburg\.de\.*/;
+var startUrl    = 'http://www.uni-regensburg.de';
+// var startUrl = 'http://localhost:3000';
 
+crawler.maxQueueLength = 1000;
 crawler.onUrlFetched = function(currentUrl, res) {
 	if (res.type !== 'text/html') {
 		return;
@@ -24,22 +31,33 @@ crawler.onUrlFetched = function(currentUrl, res) {
 		return;
 	}
 
-	if (numDocs >= queueMax) {
+	if (numDocs >= indexMax) {
 		crawler.stop();
 		return;
 	}
 
-	numDocs++;
+	// var linksRegex = /<a href=("|')(.*?)("|')/g;
+	// var link;
+
+	// while (link = linksRegex.exec(res.text)) {
+	// 	var urlToFollow = url.resolve(currentUrl, link[2].split('#')[0]);
+	// 	if (domain.test(urlToFollow)) {
+	// 		var parsedUrl = url.parse(urlToFollow);
+	// 		crawler.queue(parsedUrl.protocol + '//' + parsedUrl.host + parsedUrl.pathname);
+	// 		numQueued++;
+	// 	}
+	// }
 
 	var $ = cheerio.load(res.text);
 
 	if (followLinks) {
 		$('a').each(function(index, a) {
-			var href = $(a).attr('href');
+			var href = unleakString($(a).attr('href'));
 			if (href) {
-				var nextLink = url.resolve(currentUrl, href.split('#')[0]);
-				if (/(http:\/\/|https:\/\/)(.*)\.uni-regensburg\.de\.*/.test(nextLink)) {
-					crawler.queue(nextLink);
+				var nextLink = url.resolve(currentUrl, unleakString(href.split('#')[0]));
+				if (domain.test(nextLink)) {
+					var parsedUrl = url.parse(nextLink);
+					crawler.queue(parsedUrl.protocol + '//' + parsedUrl.host + parsedUrl.pathname);
 					numQueued++;
 				}
 			}
@@ -47,6 +65,8 @@ crawler.onUrlFetched = function(currentUrl, res) {
 	}
 
 	$('.navigation').remove();
+	$('script').remove();
+	$('style').remove();
 	$('.menu-left').remove();
 
 	var body = {
@@ -54,21 +74,31 @@ crawler.onUrlFetched = function(currentUrl, res) {
 			doc: {
 				id: currentUrl,
 				url: currentUrl,
-				title: $('title').text(),
-				content: $('body').text().split(/\s+/).join(' ').trim(),
-				description: $('meta[name=description]').attr('content'),
-				keywords: $('meta[name=keywords]').attr('content')
+				title: unleakString($('title').text()),
+				content: unleakString($('body').text().split(/\s+/).join(' ').trim()),
+				description: unleakString($('meta[name=description]').attr('content')),
+				keywords: unleakString($('meta[name=keywords]').attr('content'))
 			}
 		}
 	};
 
+	numDocs++;
+
 	request
 		.post('localhost:8983/solr/update/?commit=true')
 		.send(body)
-		.end(function(res) {
+		.end(function(err, res) {
 			numPushed++;
-			console.log('(' + numPushed + '/' + numDocs + ') ' + res.statusCode + ' ' + currentUrl);
+			if (err) {
+				console.log('!!! Error pushing to solr. URL: ' + currentUrl);
+				console.log(err);
+				return;
+			}
+
+			console.log('(' + numDocs + ' ' + ' - ' + crawler.numFetched() + ' / ' + crawler.queueLength() + ') ' + currentUrl);
 		});
 };
 
-crawler.queue('http://www.uni-regensburg.de');
+// crawler.queue('http://www-verwaltung.uni-regensburg.de/B//telefon.htm');
+crawler.queue(startUrl);
+function unleakString(s) { return (' ' + s).replace(/^\s/, ''); }
