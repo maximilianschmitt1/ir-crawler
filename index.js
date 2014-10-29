d88a0aec2f10587e74694272fdbf7fbb29d6936e
Maximilian Schmitt <maximilian.schmitt@stud.uni-regensburg.de>
2014-10-29 14:02:21 +0100

    optimisations

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 'use strict';
 
-var agent = require('webkit-devtools-agent');
-agent.start();
+var memwatch = require('memwatch');
+var heapdump = require('heapdump');
+
+memwatch.on('leak', function(info) {
+	console.log('Memory leak detected: ' + info.reason);
+	heapdump.writeSnapshot();
+});
 
 var request     = require('superagent');
 var cheerio     = require('cheerio');
-var crawler     = require('./crawler');
 var url         = require('url');
+var crawler     = require('./crawler');
 
 var followLinks = true;
 var numDocs     = 0;
-var numPushed   = 0;
-var numQueued   = 0;
 var indexMax    = 24000;
 var domain      = /(http:\/\/|https:\/\/)(.*)\.uni-regensburg\.de\.*/;
 var startUrl    = 'http://www.uni-regensburg.de';
-// var startUrl = 'http://localhost:3000';
 
 crawler.maxQueueLength = 1000;
+
 crawler.onUrlFetched = function(currentUrl, res) {
-	if (res.type !== 'text/html') {
-		return;
-	}
-
-	if (typeof res.text === 'undefined') {
-		return;
-	}
-
-	if (res.statusCode !== 200) {
+	if (!isValidResponse(res)) {
 		return;
 	}
 
@@ -36,69 +31,96 @@ crawler.onUrlFetched = function(currentUrl, res) {
 		return;
 	}
 
-	// var linksRegex = /<a href=("|')(.*?)("|')/g;
-	// var link;
-
-	// while (link = linksRegex.exec(res.text)) {
-	// 	var urlToFollow = url.resolve(currentUrl, link[2].split('#')[0]);
-	// 	if (domain.test(urlToFollow)) {
-	// 		var parsedUrl = url.parse(urlToFollow);
-	// 		crawler.queue(parsedUrl.protocol + '//' + parsedUrl.host + parsedUrl.pathname);
-	// 		numQueued++;
-	// 	}
-	// }
-
-	var $ = cheerio.load(res.text);
+	numDocs++;
 
 	if (followLinks) {
-		$('a').each(function(index, a) {
-			var href = unleakString($(a).attr('href'));
-			if (href) {
-				var nextLink = url.resolve(currentUrl, unleakString(href.split('#')[0]));
-				if (domain.test(nextLink)) {
-					var parsedUrl = url.parse(nextLink);
-					crawler.queue(parsedUrl.protocol + '//' + parsedUrl.host + parsedUrl.pathname);
-					numQueued++;
-				}
-			}
-		});
+		queueLinks(res.text, currentUrl);
 	}
+
+	postToSolr(res.text, currentUrl);
+};
+
+crawler.queue(startUrl);
+
+function isValidResponse(res) {
+	return (
+		res.type === 'text/html' &&
+		typeof res.text !== 'undefined' &&
+		res.statusCode === 200
+	);
+}
+
+function queueLinks(html, currentUrl) {
+	var links = [];
+
+	if (followLinks) {
+		links = extractLinks(html, currentUrl);
+		links.forEach(crawler.queue.bind(crawler));
+	}
+}
+
+function extractLinks(html, currentUrl) {
+	var $ = cheerio.load(html);
+	var links = [];
+
+	$('a').each(function(index, a) {
+		var href = unleakString($(a).attr('href'));
+		if (href) {
+			var nextLink = url.resolve(currentUrl, unleakString(href.split('#')[0]));
+			if (domain.test(nextLink)) {
+				var parsedUrl = url.parse(nextLink);
+				links.push(parsedUrl.protocol + '//' + parsedUrl.host + parsedUrl.pathname);
+			}
+		}
+	});
+
+	return links;
+}
+
+function extractRelevantContent(html, currentUrl) {
+	var $ = cheerio.load(html);
 
 	$('.navigation').remove();
 	$('script').remove();
 	$('style').remove();
 	$('.menu-left').remove();
 
-	var body = {
+	var title       = unleakString($('title').text());
+	var content     = unleakString($('body').text().split(/\s+/).join(' ').trim());
+	var description = unleakString($('meta[name=description]').attr('content'));
+	var keywords    = unleakString($('meta[name=keywords]').attr('content'));
+
+	return {
+		id: currentUrl,
+		url: currentUrl,
+		title: title,
+		content: content,
+		description: description,
+		keywords: keywords
+	};
+}
+
+function postToSolr(html, currentUrl) {
+	var solrRequest = {
 		add: {
-			doc: {
-				id: currentUrl,
-				url: currentUrl,
-				title: unleakString($('title').text()),
-				content: unleakString($('body').text().split(/\s+/).join(' ').trim()),
-				description: unleakString($('meta[name=description]').attr('content')),
-				keywords: unleakString($('meta[name=keywords]').attr('content'))
-			}
+			doc: extractRelevantContent(html, currentUrl)
 		}
 	};
 
-	numDocs++;
-
 	request
 		.post('localhost:8983/solr/update/?commit=true')
-		.send(body)
+		.send(solrRequest)
 		.end(function(err, res) {
-			numPushed++;
 			if (err) {
-				console.log('!!! Error pushing to solr. URL: ' + currentUrl);
+				console.log('!!! Error posting to solr. URL: ' + currentUrl);
 				console.log(err);
 				return;
 			}
 
 			console.log('(' + numDocs + ' ' + ' - ' + crawler.numFetched() + ' / ' + crawler.queueLength() + ') ' + currentUrl);
 		});
-};
+}
 
-// crawler.queue('http://www-verwaltung.uni-regensburg.de/B//telefon.htm');
-crawler.queue(startUrl);
-function unleakString(s) { return (' ' + s).replace(/^\s/, ''); }
+function unleakString(s) {
+	return (' ' + s).replace(/^\s/, '');
+}
